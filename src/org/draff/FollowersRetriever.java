a8e05a7205ce953ae6d6bd507d9c644946a03cb7
draffensperger <d.raffensperger@gmail.com>
2016-01-01 16:18:47 -0500

    Enable basic storing of followers

--- src/org/draff/FollowersRetriever.java
+++ src/org/draff/FollowersRetriever.java
@@ -2,23 +2,34 @@ package org.draff;
 
 import twitter4j.*;
 import twitter4j.api.FriendsFollowersResources;
+
+import com.google.api.services.datastore.DatastoreV1.*;
+import com.google.api.services.datastore.client.Datastore;
+import com.google.api.services.datastore.client.DatastoreException;
+import com.google.api.services.datastore.client.DatastoreFactory;
+import com.google.api.services.datastore.client.DatastoreHelper;
+import com.google.protobuf.ByteString;
+
+import static com.google.api.services.datastore.client.DatastoreHelper.*;
+
 /**
  * Created by dave on 12/28/15.
  */
 public class FollowersRetriever {
   private FriendsFollowersResources friendsFollowers;
-  public FollowersRetriever(FriendsFollowersResources friendsFollowers) {
+  private Datastore datastore;
+  public FollowersRetriever(FriendsFollowersResources friendsFollowers, Datastore datastore) {
     this.friendsFollowers = friendsFollowers;
+    this.datastore = datastore;
   }
 
-  public void retrieveFollowers(String screenName) throws TwitterException {
-    // The Twitter4J docs say for the cursor to start at -1
+  public void retrieveFollowers(long userId) throws TwitterException {
     boolean hasNext = true;
     long cursor = -1;
     IDs ids;
     while(hasNext) {
-      ids = friendsFollowers.getFollowersIDs(screenName, cursor);
-      saveIds(screenName, ids.getIDs());
+      ids = friendsFollowers.getFollowersIDs(userId, cursor);
+      saveFollowers(userId, ids.getIDs());
       hasNext = ids.hasNext();
       if (hasNext) {
         cursor = ids.getNextCursor();
@@ -26,9 +37,45 @@ public class FollowersRetriever {
     }
   }
 
-  private void saveIds(String screenName, long[] ids) {
-    for(long id: ids) {
-      System.out.println("Follower of " + screenName + ": " + id);
+  private void saveUser(long userId) {
+
+  }
+
+  private void saveFollowers(long userId, long[] followerIds) {
+    long retrievedAt = System.currentTimeMillis();
+
+    Entity[] followers = new Entity[followerIds.length];
+    for (int i = 0; i < followers.length; i++) {
+      long followerId = followerIds[i];
+      followers[i] = buildTwitterFollower(userId, followerId, retrievedAt);
+    }
+    saveUpserts(followers);
+  }
+
+  private Entity buildTwitterFollower(long userId, long followerId, long retrievedAt) {
+    return Entity.newBuilder()
+        .setKey(makeKey("TwitterFollower", userId + ":" + followerId))
+        .addProperty(makeProperty("userId", makeValue(userId)))
+        .addProperty(makeProperty("followerId", makeValue(followerId)))
+        .addProperty(makeProperty("retrievedAt", makeValue(retrievedAt)))
+        .build();
+  }
+
+  private void saveUpserts(Entity[] entities) {
+    Mutation.Builder mutation = Mutation.newBuilder();
+    for (Entity entity : entities) {
+      mutation.addUpsert(entity);
+    }
+
+    CommitRequest request = CommitRequest.newBuilder()
+        .setMode(CommitRequest.Mode.NON_TRANSACTIONAL)
+        .setMutation(mutation)
+        .build();
+
+    try {
+      datastore.commit(request);
+    } catch (DatastoreException e) {
+      e.printStackTrace();
     }
   }
 }
--- src/org/draff/Main.java
+++ src/org/draff/Main.java
@@ -6,6 +6,7 @@ import twitter4j.*;
 import java.util.*;
 
 import org.apache.log4j.PropertyConfigurator;
+import org.omg.CORBA.DATA_CONVERSION;
 
 import com.google.api.services.datastore.DatastoreV1.*;
 import com.google.api.services.datastore.client.Datastore;
@@ -21,23 +22,21 @@ public class Main {
   public static void main(String[] args) throws TwitterException {
     setupLogger();
 
-    testDatastore("twitter-communities");
-    //retrieveFollowers();
+    //testDatastore();
+    retrieveFollowers();
   }
 
   private static void retrieveFollowers() throws TwitterException {
     Twitter twitter = new TwitterFactory().getInstance();
-    FollowersRetriever retriever = new FollowersRetriever(twitter.friendsFollowers());
-    retriever.retrieveFollowers("praffensperger");
+    Datastore datastore = getDatastore();
+    FollowersRetriever retriever = new FollowersRetriever(twitter.friendsFollowers(), datastore);
+    retriever.retrieveFollowers(341522259);
   }
 
-  private static void testDatastore(String datasetId) {
+  private static Datastore getDatastore() {
     Datastore datastore = null;
     try {
-      // Setup the connection to Google Cloud Datastore and infer credentials
-      // from the environment.
-      datastore = DatastoreFactory.get().create(DatastoreHelper.getOptionsFromEnv()
-          .dataset(datasetId).build());
+      datastore = DatastoreFactory.get().create(DatastoreHelper.getOptionsFromEnv().build());
     } catch (GeneralSecurityException exception) {
       System.err.println("Security error connecting to the datastore: " + exception.getMessage());
       exception.printStackTrace();
@@ -47,72 +46,47 @@ public class Main {
       exception.printStackTrace();
       System.exit(1);
     }
+    return datastore;
+  }
 
+  private static void testDatastore() {
+    Datastore datastore = getDatastore();
     try {
-      // Create an RPC request to begin a new transaction.
       BeginTransactionRequest.Builder treq = BeginTransactionRequest.newBuilder();
-      // Execute the RPC synchronously.
       BeginTransactionResponse tres = datastore.beginTransaction(treq.build());
-      // Get the transaction handle from the response.
       ByteString tx = tres.getTransaction();
 
-      // Create an RPC request to get entities by key.
       LookupRequest.Builder lreq = LookupRequest.newBuilder();
-      // Set the entity key with only one `path_element`: no parent.
       Key.Builder key = Key.newBuilder().addPathElement(
           Key.PathElement.newBuilder()
           .setKind("Trivia")
           .setName("hgtg"));
-      // Add one key to the lookup request.
       lreq.addKey(key);
-      // Set the transaction, so we get a consistent snapshot of the
-      // entity at the time the transaction started.
       lreq.getReadOptionsBuilder().setTransaction(tx);
-      // Execute the RPC and get the response.
       LookupResponse lresp = datastore.lookup(lreq.build());
-      // Create an RPC request to commit the transaction.
+
       CommitRequest.Builder creq = CommitRequest.newBuilder();
-      // Set the transaction to commit.
       creq.setTransaction(tx);
       Entity entity;
       if (lresp.getFoundCount() > 0) {
         entity = lresp.getFound(0).getEntity();
       } else {
-        // If no entity was found, create a new one.
         Entity.Builder entityBuilder = Entity.newBuilder();
-        // Set the entity key.
         entityBuilder.setKey(key);
-        // Add two entity properties:
-        // - a utf-8 string: `question`
         entityBuilder.addProperty(Property.newBuilder()
             .setName("question")
             .setValue(Value.newBuilder().setStringValue("Meaning of Life?")));
-        // - a 64bit integer: `answer`
         entityBuilder.addProperty(Property.newBuilder()
             .setName("answer")
             .setValue(Value.newBuilder().setIntegerValue(42)));
-        // Build the entity.
         entity = entityBuilder.build();
-        // Insert the entity in the commit request mutation.
         creq.getMutationBuilder().addInsert(entity);
       }
-      // Execute the Commit RPC synchronously and ignore the response.
-      // Apply the insert mutation if the entity was not found and close
-      // the transaction.
       datastore.commit(creq.build());
-      // Get `question` property value.
       String question = entity.getProperty(0).getValue().getStringValue();
-      // Get `answer` property value.
       Long answer = entity.getProperty(1).getValue().getIntegerValue();
       System.out.println(question);
-      //String result = System.console().readLine("> ");
-      String result = "hi";
-      if (result.equals(answer.toString())) {
-        System.out.println("fascinating, extraordinary and, " +
-            "when you think hard about it, completely obvious.");
-      } else {
-        System.out.println("Don't Panic!");
-      }
+      System.out.println(answer.toString());
     } catch (DatastoreException exception) {
       // Catch all Datastore rpc errors.
       System.err.println("Error while doing datastore operation");
