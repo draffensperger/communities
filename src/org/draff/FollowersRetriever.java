0fb81f66d4aad4cd4163ac8c19cfb5b66cad26aa
draffensperger <d.raffensperger@gmail.com>
2016-01-01 19:51:52 -0500

    Started on the task retrieval logic

--- src/org/draff/FollowersRetriever.java
+++ src/org/draff/FollowersRetriever.java
@@ -3,9 +3,13 @@ package org.draff;
 import twitter4j.*;
 import twitter4j.api.FriendsFollowersResources;
 
+import com.google.api.services.datastore.DatastoreV1;
 import com.google.api.services.datastore.DatastoreV1.*;
 import com.google.api.services.datastore.client.Datastore;
 import com.google.api.services.datastore.client.DatastoreException;
+import java.util.List;
+import java.util.Map;
+
 import com.google.api.services.datastore.client.DatastoreFactory;
 import com.google.api.services.datastore.client.DatastoreHelper;
 import com.google.protobuf.ByteString;
@@ -24,7 +28,33 @@ public class FollowersRetriever {
   }
 
   public void retrieveFollowers(long userId) throws TwitterException {
-    boolean hasNext = true;
+    try {
+      saveUser(userId, 2, false, -1);
+      doFollowersRetrieval();
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void doFollowersRetrieval() throws TwitterException, DatastoreException {
+    Filter notStarted = makeFilter("followerRetrievalStarted", PropertyFilter.Operator.EQUAL, makeValue(false)).build();
+    DatastoreV1.Query.Builder q = DatastoreV1.Query.newBuilder();
+    q.addKindBuilder().setName("TwitterUser");
+    q.setFilter(notStarted);
+    RunQueryRequest request = RunQueryRequest.newBuilder().setQuery(q.build()).build();
+    RunQueryResponse response = datastore.runQuery(request);
+
+    List<EntityResult> results = response.getBatch().getEntityResultList();
+
+    for (EntityResult result : results) {
+      Entity entity = result.getEntity();
+      Map<String, Value> props = getPropertyMap(entity);
+      long userId = entity.getKey().getPathElement(0).getId();
+      long depthGoal = getLong(props.get("followerDepthGoal"));
+      long cursor = getLong(props.get("followersCursor"));
+    }
+
+/*    boolean hasNext = true;
     long cursor = -1;
     IDs ids;
     while(hasNext) {
@@ -34,11 +64,19 @@ public class FollowersRetriever {
       if (hasNext) {
         cursor = ids.getNextCursor();
       }
-    }
+    }*/
   }
 
-  private void saveUser(long userId) {
-
+  private void saveUser(long userId, int followerDepthGoal, boolean followerRetrievalStarted, long followersCursor) {
+    long updatedAt = System.currentTimeMillis();
+    Entity user = Entity.newBuilder()
+        .setKey(makeKey("TwitterUser", userId))
+        .addProperty(makeProperty("followerDepthGoal", makeValue(followerDepthGoal)))
+        .addProperty(makeProperty("followerRetrievalStarted", makeValue(followerRetrievalStarted)))
+        .addProperty(makeProperty("followersCursor", makeValue(followersCursor)))
+        .addProperty(makeProperty("updatedAt", makeValue(updatedAt)))
+        .build();
+    saveUpsert(user);
   }
 
   private void saveFollowers(long userId, long[] followerIds) {
@@ -61,12 +99,19 @@ public class FollowersRetriever {
         .build();
   }
 
+  private void saveUpsert(Entity entity) {
+    saveMutation(Mutation.newBuilder().addUpsert(entity));
+  }
+
   private void saveUpserts(Entity[] entities) {
     Mutation.Builder mutation = Mutation.newBuilder();
     for (Entity entity : entities) {
       mutation.addUpsert(entity);
     }
+    saveMutation(mutation);
+  }
 
+  private void saveMutation(Mutation.Builder mutation) {
     CommitRequest request = CommitRequest.newBuilder()
         .setMode(CommitRequest.Mode.NON_TRANSACTIONAL)
         .setMutation(mutation)
